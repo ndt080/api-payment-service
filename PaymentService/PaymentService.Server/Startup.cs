4dc2ea219e252383c25507f2af9c17333d7dd3d0
ndt080 <68751397+ndt080@users.noreply.github.com>
2021-12-08 02:45:04 +0300

    update

--- PaymentService/PaymentService.Server/Startup.cs
+++ PaymentService/PaymentService.Server/Startup.cs
@@ -45,12 +45,15 @@ namespace PaymentService.Server
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PaymentService.Server v1"));
-            }
+            // if (env.IsDevelopment())
+            // {
+            //     app.UseDeveloperExceptionPage();
+            //     app.UseSwagger();
+            //     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PaymentService.Server v1"));
+            // }
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PaymentService.Server v1"));
 
             app.UseHttpsRedirection();
             app.UseRouting();
